bd519b190c154f8a846ca58e76049a36c96cce35
N.Ardi <noone.nu1@gmail.com>
2015-07-15 06:42:20 +0700

    add test to return all data

--- test/test.js
+++ test/test.js
@@ -1,9 +1,15 @@
 var assert = require("assert");
 var Model = require("../Model");
+
+var alldata = {name: 'Ardi'};
 var model = new Model({name:'Ardi'});
 
 describe('Model', function(){
-    it('should return my name', function(){
+    it('should return all data', function(){
+        assert.deepEqual(alldata, model.get())
+    });
+
+    it('should return single data', function(){
         assert.equal('Ardi', model.get('name'))
     });
 
